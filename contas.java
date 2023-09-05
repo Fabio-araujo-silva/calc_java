4ccd0072c7bbdf09466c238dab07a8e0aebfec63
Fabio Araujo <82675020+Fabio-araujo-silva@users.noreply.github.com>
2023-09-05 17:17:17 -0300

    Create contas.java

---
+++ contas.java
@@ -0,0 +1,24 @@
+public class contas {
+  
+ static void soma(int a,int b){
+   int c = a+b; 
+   System.out.println("soma é "+c);
+  }
+  static void subtracao(int a,int b){
+   int c = a-b; 
+   System.out.println("subtracao é "+c);
+  }
+  static void multiplicacao(int a,int b){
+   int c = a*b; 
+   System.out.println("multiplicacao é "+c);
+  }
+  static void divisao(double a,double b){
+   double c = a/b; 
+   System.out.println("divisao é "+c);
+  }
+  static void exponenciacao(double a,double b){
+   double c = Math.pow(a,b); 
+   System.out.println("exponenciação é "+c);
+  }
+  
+}
